c6d0b2746bc821c1677337678df99d8de88aee84
Pralhad <mrpatilpralhad@gmail.com>
2017-07-14 23:27:13 +0530

    added dice component

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-var canvas;
-var ctx;
+
 var dimension = 600;
 var noOfLinesToBeDrawn = 5;
 //var height = 600;
@@ -9,7 +8,22 @@ var USED_AVATAR_ID_LIST = new Array();
 //special cells
 //(0,2) (2,0) (2,2) (4,2) (2,4)
 var specialCellList = new Array("0,2","2,0","2,2","4,2","2,4");
-
+/*
+				Path3
+		     _  _  _   _  _
+		 	|00 01 02 03 04|
+			|10 11 12 13 14|
+	   Path4|20 21 22 23 24| Path2
+			|30 31 32 33 34|
+			|40 41 42 43 44|
+				  Path1
+*/
+var PATHS_LIST = [
+					["42","43","44","34","24","14","04","03","02","01","00","10","20","30","40","41","31","21","11","12","13","23","33","32","22"],//path1
+					["24","14","04","03","02","01","00","10","20","30","40","41","42","43","44","34","33","32","31","21","11","12","13","23","22"],//path2
+					["02","01","00","10","20","30","40","41","42","43","44","34","24","14","04","03","13","23","33","32","31","21","11","12","22"],//path3
+					["20","30","40","41","42","43","44","34","24","14","04","03","02","01","00","10","11","12","13","23","33","32","31","21","22"] //path4
+				 ];
 //TODO allow spectators, they can join a player, they should also be able to join game in case some one becomes inactive
 
 var PLAYER_ITEM_STATE = {
@@ -31,19 +45,27 @@ var BOARD_STATE = {
 };
 var FINISHED_COUNT = 0;
 
+var DICE = new Array();// array which hold state of each dice item
 window.onload = function(){
 	
 	//code to draw the board
-    canvas = document.getElementById('game-board');
-    ctx = canvas.getContext('2d');
-    //drawBoard();
 	drawTableBoard();
 	addPlayerToGame(new Player("Pralhad","10"));
 	addPlayerToGame(new Player("Pralhad","11"));
 	addPlayerToGame(new Player("Pralhad","12"));
 	addPlayerToGame(new Player("Pralhad","13"));
-	console.log(PLAYERS_LIST);
-	console.log(USED_AVATAR_ID_LIST);
+//	console.log(PLAYERS_LIST);
+//	console.log(USED_AVATAR_ID_LIST);
+//  testPlayerPath(PLAYERS_LIST[0]);
+
+}
+
+function testPlayerPath(player){
+	for(let j = 0 ; j < 25 ; j++){
+		let cellId = PATHS_LIST[player.homeIndex][j];
+		let currentCell = document.getElementById(cellId);
+		currentCell.innerHTML = j;
+	}
 }
 
 function drawTableBoard(){
@@ -53,6 +75,7 @@ function drawTableBoard(){
 		for(let j = 0 ; j < noOfLinesToBeDrawn ; j++){
 			let td = document.createElement("td");
 			td.className = "cell";
+			td.id = i+""+j;
 			if(i === 2 || j === 2){
 				let searchString = i + "," + j;
 				if(specialCellList.indexOf(searchString) !== -1){
@@ -88,13 +111,33 @@ function addPlayerToGame(personObject){
 var Player = function(name,avatarId){
 	let personId = PERSON_COUNT++;
 	let playerItemList = new Array();
+	let homeIndex = 0;
+	
+	//block to assign home index
+	switch(personId){
+		case 0 :
+			homeIndex = 0;
+			break;
+		case 1 :
+			homeIndex = 2;
+			break;
+		case 2 :
+			homeIndex = 1;
+			break;
+		case 3 :
+			homeIndex = 3;
+			break;
+	}
+			
+	//block to create person play items
 	for(let i = 0; i < 4;i++)
 		playerItemList.push(new PlayItem(avatarId,personId));
 	return {
 		"name":name,
 		"id":personId,
 		"playerItemList":playerItemList,
-		"playerState" : PLAYER_STATE.JOINED
+		"playerState" : PLAYER_STATE.JOINED,
+		"homeIndex" : homeIndex
 	};
 }
 
@@ -107,37 +150,24 @@ var PlayItem = function(avatarId,personId){
 		"avatarId" : avatarId,
 		"personId" : personId,
 		"statePosition" : 0,
-		"uiX" : 0,
-		"uiY" : 0,
+		"distanceFromHome" : 0,
 		"itemState" : PLAYER_ITEM_STATE.HOME
 	};
 }
 
-/*
-	Function to draw line between 2 points
-*/
-//function drawLine( fromX, fromY,  toX,  toY){
-//    ctx.moveTo(fromX, fromY);
-//    ctx.lineTo(toX,toY);
-//    ctx.stroke();
-//}
-
-//function drawBoard(){
-//	canvas.width = canvas.height = dimension;
-//    var offset = dimension / noOfLinesToBeDrawn;
-//    var startX = canvas.offsetLeft;
-//    var startY = canvas.offsetTop;
-//    
-//    for(var i = 0; i <= noOfLinesToBeDrawn; i++){
-//        drawLine(startX + (offset * i),
-//                 startY,
-//                 startX + (offset * i),
-//                 startY + canvas.height
-//                );
-//		drawLine(startX,
-//				 startY + (offset * i),
-//				 startX + canvas.width,
-//				 startY + (offset * i)
-//				);
-//    }
-//}
+function rollDice(){
+	DICE.length = 0; // clear the array
+	for(let i = 0 ; i < 4; i++) {// there will be 4 dice
+		DICE[i] = Math.floor((Math.random() * 10 ) % 2);
+	}
+	
+	let value = 0;
+	for(let i = 0 ; i < 4; i++) {// there will be 4 dice
+		if(DICE[i] === 1) // 1 is white part
+			value++;
+	}
+	if(value === 0 ) // all are black means it is 8
+		value = 8;
+	console.log(DICE);
+	console.log(value);
+}
